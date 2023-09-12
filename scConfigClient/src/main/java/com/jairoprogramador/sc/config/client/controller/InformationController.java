d9db71773c2f74de2be04ca4cdeb7ffba7c18223
Jairo Galvez <jgalvez@baufest.com>
2023-09-11 21:45:13 -0500

    repo git and config server, client

--- scConfigClient/src/main/java/com/jairoprogramador/sc/config/client/controller/InformationController.java
+++ scConfigClient/src/main/java/com/jairoprogramador/sc/config/client/controller/InformationController.java
@@ -1,2 +1,16 @@
-package com.jairoprogramador.sc.config.client.controller;public class InformationController {
+package com.jairoprogramador.sc.config.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InformationController {
+    @Value("${info.data:default}")
+    private String data;
+
+    @GetMapping("properties")
+    public String viewDiscounts() {
+        return "properties is " + data;
+    }
 }
