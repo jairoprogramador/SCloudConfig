ed7316b69f6064c9b584a16c9b0b969abf6fadc0
Jairo Galvez <jairo.jggc@gmail.com>
2023-09-18 17:31:24 -0500

    add mvn

--- scConfigClient/src/main/java/com/jairoprogramador/sc/config/client/controller/InformationController.java
+++ scConfigClient/src/main/java/com/jairoprogramador/sc/config/client/controller/InformationController.java
@@ -1,7 +1,6 @@
 package com.jairoprogramador.sc.config.client.controller;
 
 import com.jairoprogramador.sc.config.client.properties.AppProperties;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
